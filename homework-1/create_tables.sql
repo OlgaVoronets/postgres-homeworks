60b898524f77682793ac0d2fa7690d2b03f8c831
olga voronets <voronets80@gmail.com>
2023-11-02 19:50:00 +0300

    Sql-код для создания таблиц скопирован в файл `create_tables.sql`

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,26 @@
 -- SQL-команды для создания таблиц
+CREATE TABLE employees
+(
+	employee_id serial PRIMARY KEY NOT NULL,
+    first_name varchar(100),
+	last_name varchar(100),
+	title varchar(100),
+	birth_date date,
+	notes text
+);
+
+CREATE TABLE customers
+(
+	customer_id varchar(10) PRIMARY KEY NOT NULL,
+	company_name text NOT NULL,
+	contact_name varchar(100) NOT NULL
+);
+
+CREATE TABLE orders
+(
+	order_id int PRIMARY KEY NOT NULL,
+	customer_id varchar(10) REFERENCES customers(customer_id),
+	employee_id int REFERENCES employees(employee_id),
+	order_date date NOT NULL,
+	ship_city varchar(100) NOT NULL
+);
